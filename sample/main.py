4228efba4866e541223d5d060621014ac3c5857a
Ludy <1@astra-g.org>
2021-12-29 13:48:23 +0100

    Update main.py

--- sample/main.py
+++ sample/main.py
@@ -1,7 +1,7 @@
-import pyxplora_api as PXA
+import pyxplora_api.pyxplora_api as PyXploraApi
 
 def main():
-    xplora = PXA.PyXploraApi("+49", "123456789", "ACCOUNT_PASSWORT", "de-DE", "Europe/Berlin")
+    xplora = PyXploraApi("+49", "123456789", "ACCOUNT_PASSWORT", "de-DE", "Europe/Berlin")
     print(xplora.getUserName())
 
 if __name__ == '__main__':
