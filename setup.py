86af66b5fefd0eb4bbd0f992cf9cb96bb5a7c562
Ludy87 <Ludy87@users.noreply.github.com>
2022-04-03 12:25:29 +0200

    1.0.77
    Signed-off-by: Ludy87 <Ludy87@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyxplora_api",
-    version="1.0.76",
+    version="1.0.77",
     author="Ludy87",
     author_email="android@astra-g.org",
     description="Python Xplora® Api",
