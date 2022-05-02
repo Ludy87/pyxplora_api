14506334a9bc6a132b0a274bfa609095a762e063
Ludy87 <Ludy87@users.noreply.github.com>
2022-05-02 18:44:59 +0200

    🌱 new Errors - NoAdminError - ChildNoError - FunctionError
    Signed-off-by: Ludy87 <Ludy87@users.noreply.github.com>

--- pyxplora_api/exception_classes.py
+++ pyxplora_api/exception_classes.py
@@ -5,11 +5,36 @@ class Error(Exception):
     pass
 
 
+class NoAdminError(Error):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def __str__(self) -> str:
+        return "no Admin"
+
+
+class ChildNoError(Error):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def __str__(self) -> str:
+        return "Child phonenumber or Watch ID not found!"
+
+
+class FunctionError(Error):
+    def __init__(self, fnc: str) -> None:
+        self.fnc = fnc
+        super().__init__(self.fnc)
+
+    def __str__(self) -> str:
+        return "Xplora API call finally failed with response: {0}".format(self.fnc)
+
+
 class LoginError(Error):
-    def __init__(self, message, res=1):
+    def __init__(self, message: str, res: int = 1) -> None:
         self.message = message
         self.res = res
         super().__init__(self.message, self.res)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.message} - {self.res}"
