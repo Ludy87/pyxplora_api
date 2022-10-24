6795b03026c693eeeb063a6f63dc7c40e6c6662c
Ludy87 <Ludy87@users.noreply.github.com>
2022-10-24 21:53:17 +0200

    Update const.py

--- src/pyxplora_api/const.py
+++ src/pyxplora_api/const.py
@@ -1,5 +1,5 @@
-VERSION = "2.2.1"
-VERSION_APP = "1278"
+VERSION = "2.2.2"
+VERSION_APP = "1280"
 API_KEY = "fc45d50304511edbf67a12b93c413b6a"
 API_SECRET = "1e9b6fe0327711ed959359c157878dcb"
 ENDPOINT = "https://api.myxplora.com/api"
