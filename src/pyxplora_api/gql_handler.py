0907b08a7213540da573f08dc07efa2b706931eb
Ludy87 <Ludy87@users.noreply.github.com>
2022-10-25 19:26:24 +0200

    Update gql_handler.py

--- src/pyxplora_api/gql_handler.py
+++ src/pyxplora_api/gql_handler.py
@@ -5,7 +5,7 @@ from python_graphql_client import GraphqlClient
 from .const import ENDPOINT
 from .exception_classes import ErrorMSG, LoginError, NoAdminError
 from .handler_gql import HandlerGQL
-from .status import NormalStatus, YesOrNo
+from .status import NormalStatus, UserContactType, YesOrNo
 
 from . import gql_mutations as gm
 from . import gql_queries as gq
@@ -319,10 +319,10 @@ class GQLHandler(HandlerGQL):
                 self.errors.append({"function": "getEndTrackingWatch", "error": error})
         return data.get("data", {})
 
-    def checkEmailOrPhoneExist(self, type: str, email: str, countryCode: str, phoneNumber: str) -> dict[str, bool]:
+    def checkEmailOrPhoneExist(self, type: UserContactType, email: str, countryCode: str, phoneNumber: str) -> dict[str, bool]:
         data = self.runAuthorizedGqlQuery(
             gq.WATCH_Q.get("checkEmailOrPhoneExistQ", ""),
-            {"type": type, "email": email, "countryCode": countryCode, "phoneNumber": phoneNumber},
+            {"type": type.value, "email": email, "countryCode": countryCode, "phoneNumber": phoneNumber},
         )
         return data.get("data", {})
 
