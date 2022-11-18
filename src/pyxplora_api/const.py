0bbd0d7e37a2a65be9658d5e81d2819e25b5e025
Ludy <Ludy87@users.noreply.github.com>
2022-11-18 19:32:38 +0100

    ✨ v2.3.3-1283 (#33)
    * ✨ v2.3.3-1283
    
    Signed-off-by: Ludy87 <Ludy87@users.noreply.github.com>
    
    * Update gql_queries.py
    
    Signed-off-by: Ludy87 <Ludy87@users.noreply.github.com>

--- src/pyxplora_api/const.py
+++ src/pyxplora_api/const.py
@@ -1,5 +1,5 @@
-VERSION = "2.3.2"
-VERSION_APP = "1280"
+VERSION = "2.3.3"
+VERSION_APP = "1283"
 API_KEY = "fc45d50304511edbf67a12b93c413b6a"
 API_SECRET = "1e9b6fe0327711ed959359c157878dcb"
 ENDPOINT = "https://api.myxplora.com/api"
