86deee37b1161285923401e2c9d999c82b6ed81b
Ludy87 <Ludy87@users.noreply.github.com>
2022-12-18 01:05:50 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setuptools.setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Intended Audience :: Developers",
