7da83a2403b67475f56b026e247c17bc7fc76b9f
Ludy87 <Ludy87@users.noreply.github.com>
2023-03-27 16:23:21 +0200

    Update const.py

--- src/pyxplora_api/const.py
+++ src/pyxplora_api/const.py
@@ -4,6 +4,4 @@ API_KEY = "fc45d50304511edbf67a12b93c413b6a"
 API_SECRET = "1e9b6fe0327711ed959359c157878dcb"
 ENDPOINT = "https://api.myxplora.com/api"
 DEFAULT_TIMEOUT = 60
-DEFAULT_USER_AGENT = (
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
-)
+DEFAULT_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
