65da96752132362c1b42e3ebc37b9af9df9fc71d
niltrip <niltrip@web.de>
2026-07-01 00:37:37 +0200

    found at https://github.com/mspinapolice/xplora_watch_tracker
    new ENDPOINT

--- src/pyxplora_api/const.py
+++ src/pyxplora_api/const.py
@@ -1,6 +1,6 @@
-API_KEY = "fc45d50304511edbf67a12b93c413b6a"
-API_SECRET = "1e9b6fe0327711ed959359c157878dcb"
-ENDPOINT = "https://api.myxplora.com/api"
+API_KEY    = "63fa1d10289711ea80b5992f808043b2"
+API_SECRET = "27ed7670379511eab4a0f367f8eb1312"
+ENDPOINT   = "https://api.prod.myxplora.com/api"
 DEFAULT_TIMEOUT = 60
 DEFAULT_USER_AGENT = (
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.3"
